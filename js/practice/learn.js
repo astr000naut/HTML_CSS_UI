0e17da0f55770bbe8eb78cabb54a8c52b514ef57
astr000naut <10tincnh@gmail.com>
2023-04-18 11:50:49 +0700

    added convention js

---
+++ js/practice/learn.js
@@ -0,0 +1,34 @@
+// Tra ve mot Object
+document.getElementById('id')
+
+// Tra ve mot Collection
+document.getElementsByClassName('classname')
+
+
+// Tra ve mot Object
+document.querySelector('.btn')
+
+// Return mot phan tu Node list
+document.querySelectorAll('.btn')
+
+// Lay theo attribute
+document.querySelector('[attribute_name]')
+document.querySelectorAll('[attribute_name')
+
+// lay child
+document.querySelector('.page__button button')
+
+/**
+ * Lấy ra tên nhân viên
+ * @param {string} name ten nhan vien
+ * @returns {string} ten nhan vien
+ * Author: NVMANH (18/04/2023)
+ * Modifier: NVMANH(19/04/2023) - sua loi sao ten tham so
+ */
+function getName(name) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ js/practice/practice.js
@@ -0,0 +1,74 @@
+window.onload = function() {
+    // Gán sự kiện cho các thành phần:
+    initEvents();
+    //load dữ liệu cho table
+
+}
+
+
+/**
+ * Tạo các sự kiện cho các button...
+ * Author: astr000naut (18/04/2023)
+ */
+function initEvents() {
+    try {
+        // Add:
+            const btnAdd = document.querySelector("#btn-add");
+            btnAdd.addEventListener("click", btnAddOnclick); 
+        // ...
+    } catch (error) {
+        console.log(error)
+    }   
+}
+
+/**
+ * ....
+ */
+function btnAddOnclick() {
+    // Hiển thị form chi tiết nhân viên
+    document.querySelector("..").style.display = "block";
+    
+    // Lấy mã nhân viên mới và cán vào textbox mã nhân viên
+    const code = '....'
+
+    // Focus vào ô nhập liệu đầu tiên
+    
+
+    // Luu y: Co refactor code
+    
+}
+
+/**
+ * ....
+ */
+function btnSaveOnClick() {
+    // Validate dữ liệu
+    validateData();
+
+    // Nếu dữ liệu hợp lệ thì gọi api 
+
+    // Nếu dữ liệu không hợp lệ thì thông báo lỗi cụ thể
+
+}
+
+/**
+ * 
+ */
+function validateData() {
+    // Kiểm tra các dữ liệu bắt buộc nhập
+    let inputRequiredList = document.querySelectorAll('[required');
+    for (const input of inputRequiredList) {
+    
+    }
+
+    // Kiểm tra định dạng dữ liệu
+
+    // Kiểm tra ngày tháng năm sinh không được lớn hơn ngày tháng hiện tại
+    
+    // Kiểm tra độ dài chuỗi kí tự (nếu có)
+
+
+
+
+}
+ 
