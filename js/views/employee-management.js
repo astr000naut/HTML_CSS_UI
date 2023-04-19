019adf9c38a345cbe0a9f59ee6c7bfbb9e30c243
astr000naut <10tincnh@gmail.com>
2023-04-19 11:33:27 +0700

    update asterisk by using after

--- js/views/employee-management.js
+++ js/views/employee-management.js
@@ -1,62 +1,129 @@
 
 initEvents();
 
-
-/**
- * Xử lý sự kiện cho các thành phần
- * Author: astr000naut (18/04/2023)
- * Modyfied: none
- */
 function initEvents() {
   try {
 
-    // Sự kiện cho các Button
-    const addEmployeeBtn = document.querySelector('.pcontent__heading > .btn__add');
-    addEmployeeBtn.addEventListener("click", () => {toggleElement('.wrapper--form')});
+    // MAIN PAGE
+    // sidebar
 
-    const formCloseButton = document.querySelector('.form__header .btn--close');
-    formCloseButton.addEventListener("click", formCloseButtonHandler);
+    // heading 
+    
+    // content
+    initContentEvents();
+
+    // EMPLOYEE FORM 
+    initEmployeeFormEvents();
+
+    
+    // DIALOG
+    initDialogEvents();
 
   } catch (error) {
     console.log(error);
   }
 }
 
+// INIT EVENTS MAIN PAGE
 
-/**
- * Ẩn / hiện element
- * Author: astr000naut (18/04/2023)
- * Modified: none
- */
-function toggleElement(selectorParams) {
-  const element = document.querySelector(selectorParams);
-  element.classList.toggle('display--none');
+function initContentEvents() {
+
+  // main - titlebar - button
+  const addEmployeeBtn = getEl('.pcontent__heading .btn__add');
+  addEmployeeBtn.addEventListener('click', addEmployeeBtnClickHandler);
+  // main - toolbar
+
+  // main - table
+
+  // main - table - pagination
 }
 
-/**
- * Ẩn popup / form chứa nút close
- * Author: astr000naut (18/04/2023)
- * Modified: none
- */
-function hideWrapper() {
-  const closestWrapper = this.closest('.wrapper');
-  console.log(this);
-  console.log(closestWrapper);
-  console.log("--------------------------");
-  closestWrapper.classList.add('display--none');
-}
+// INIT EVENTS EMPLOYEE FORM
 
-/**
- * Sự kiện đóng form thông tin nhân viên
- * Author: astr000naut (18/04/2023)
- * Modified: none
- */
-function formCloseButtonHandler() {
-  // Popup thông báo bạn có muốn đóng không
-    const formCloseComfirmPopup = document.querySelector('.dialog--closeconfirm');
-    formCloseComfirmPopup.classList.remove('display--none');
-  //
+function initEmployeeFormEvents() {
+  // button
+  // close button
+  const formCloseBtn = getEl('.form__header .btn--close');
+  formCloseBtn.addEventListener('click', formCloseBtnClickHandler);
+  // cancel button
+  const formCancelBtn = getEl('.form__footer .footer__left .btn--secondary');
+  formCancelBtn.addEventListener('click', formCancelBtnClickHandler);
 
+  // cat button
+  const formCatBtn = getEl('.form__footer .footer__right .btn--secondary');
+  formCatBtn.addEventListener('click', formCatBtnClickHandler);
+
+  // textbox
+
+  // combobox
 
 }
 
+
+// INIT EVENTS DIALOG
+function initDialogEvents() {
+  // FORM CLOSE DIALOG
+  // formCLoseDialog close btn
+  const formCloseDialogCloseBtn = getEl('.dialog__close');
+  formCloseDialogCloseBtn.addEventListener('click', formDialogCloseBtnClickHandler);
+
+  // formCLoseDialog close btn
+  const formCloseDialogNoBtn = getEl('.dialog__footer > .btn--secondary');
+  console.log(formCloseDialogNoBtn);
+  formCloseDialogNoBtn.addEventListener('click', formDialogNoBtnClickHandler);
+  // formCLoseDialog close btn
+
+}
+
+
+
+
+// HANDLER
+
+
+function addEmployeeBtnClickHandler() {
+  showElement('.wrapper--form');
+}
+
+function formCloseBtnClickHandler() {
+  // hien thi dialog xac minh
+  showElement('.wrapper--dialog');
+
+}
+
+function formDialogCloseBtnClickHandler() {
+  hideElement('.wrapper--dialog');
+}
+
+function formDialogNoBtnClickHandler() {
+  hideElement('.wrapper--dialog');
+  hideElement('.wrapper--form');
+}
+
+function formCancelBtnClickHandler() {
+  hideElement('.wrapper--form');
+}
+
+function formCatBtnClickHandler() {
+  // ValidateInput
+  const fuIndexTxtfield = getEl('.fu__index .txtfield');
+  fuIndexTxtfield.classList.add('txtfield--error');
+  const fuIndexTxtfieldNoti = fuIndexTxtfield.children[2];
+
+}
+
+// FUNCTION
+
+function showElement(querySelectorString) {
+  const el = getEl(querySelectorString);
+  el.classList.remove('display--none');
+}
+
+function hideElement(querySelectorString) {
+  const el = getEl(querySelectorString);
+  el.classList.add('display--none');
+}
+
+function getEl(querySelectorString) {
+  return document.querySelector(querySelectorString);
+}
